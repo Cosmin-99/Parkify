41a2c8491237727a3c578bc06b0b139ee841d71d
unknown <susara.cosmin@gmail.com>
2020-04-27 00:00:30 +0300

    A small demo of app

--- src/main/java/FIS/Project/Parkify/Main.java
+++ src/main/java/FIS/Project/Parkify/Main.java
@@ -6,11 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/sample.fxml"));
+
+        URL url = new File("src/main/resources/fxml/sample.fxml").toURI().toURL();
+
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Parkify");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();

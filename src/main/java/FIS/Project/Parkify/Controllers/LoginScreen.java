847af69a47a68f748fccbab9cd4fbf8aca3ae4c6
Alin Serban <alinserban464@yahoo.com>
2020-04-29 19:02:10 +0300

    ChoiceBox and SignUp Hyperlink added

--- src/main/java/FIS/Project/Parkify/Controllers/LoginScreen.java
+++ src/main/java/FIS/Project/Parkify/Controllers/LoginScreen.java
@@ -1,16 +1,29 @@
 package FIS.Project.Parkify.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginScreen {
+
+    ObservableList<String> roleList = FXCollections.observableArrayList("Driver","Manager");
 
     @FXML
     public TextField userName;
 
     @FXML
     public PasswordField userPassword;
+
+    @FXML
+    public ChoiceBox role;
+
+    @FXML
+    private void initialize() {
+        role.setItems(roleList);
+    }
 
     @FXML
     public void LoginButtonOnClick(){

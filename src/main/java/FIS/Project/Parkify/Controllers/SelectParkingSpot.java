cd795dec0ff687406712579406ce42de2b204828
unknown <alinserban464@yahoo.com>
2020-06-15 00:01:18 +0300

    Finished branch PAR-20. Parking spots now available!

--- src/main/java/FIS/Project/Parkify/Controllers/SelectParkingSpot.java
+++ src/main/java/FIS/Project/Parkify/Controllers/SelectParkingSpot.java
@@ -1,16 +1,27 @@
 package FIS.Project.Parkify.Controllers;
 
-import FIS.Project.Parkify.Users.Driver;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
@@ -23,14 +34,60 @@ public class SelectParkingSpot {
     public Text invalid;
 
     @FXML
-    private void initialize() {}
+    public TableView<Spot> spotTable;
+    @FXML
+    public TableColumn<Spot, Integer> spotNumber;
+    @FXML
+    public TableColumn<Spot, Integer> spotFloor;
+    @FXML
+    public TableColumn<Spot, String> spotSection;
+    @FXML
+    public TableColumn<Spot, Double> spotPrice;
+    @FXML
+    public TableColumn<Spot, String> spotAvailability;
 
-    public void Reservate(){
+    private ObservableList<Spot> spots = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize(){
+        spotNumber.setCellValueFactory(new PropertyValueFactory<>("Number"));
+        spotFloor.setCellValueFactory(new PropertyValueFactory<>("Floor"));
+        spotSection.setCellValueFactory(new PropertyValueFactory<>("Section"));
+        spotPrice.setCellValueFactory(new PropertyValueFactory<>("Price"));
+        spotAvailability.setCellValueFactory(new PropertyValueFactory<>("Availability"));
 
         String hotelName = DriverHotelSelect.getHotelName();
         String hotelPath = hotelName.substring(18);
         hotelPath = "src/main/resources/Data/" + hotelPath.toLowerCase() + ".json";
-        System.out.println(hotelPath);
+
+        JSONParser parser = new JSONParser();
+        try{
+            JSONArray a = (JSONArray) parser.parse(new FileReader(hotelPath));
+
+            for (Object o : a) {
+                JSONObject spot = (JSONObject) o;
+                long number = (long) spot.get("Number");
+                long floor = (long) spot.get("Floor");
+                String section = (String) spot.get("Section");
+                double price = (double) spot.get("Price");
+                String availability = (String) spot.get("Availability");
+                spots.add(new Spot(number,floor,section,price,availability));
+            }
+
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        spotTable.setItems(spots);
+    }
+
+
+    public void reservate(){
 
         String getSpot = inputSpot.getText();
 
---
+++ src/main/java/FIS/Project/Parkify/Controllers/Spot.java
@@ -0,0 +1,81 @@
+package FIS.Project.Parkify.Controllers;
+
+import java.util.Objects;
+
+public class Spot {
+    private long number;
+    private long floor;
+    private String section;
+    private double price;
+    private String availability;
+
+    public Spot(long number,long floor, String section, double price, String availability) {
+        this.number=number;
+        this.floor=floor;
+        this.section=section;
+        this.price=price;
+        this.availability=availability;
+    }
+
+    @Override
+    public String toString() {
+        return "Spot: " + number + ", Floor: " + floor + ", Section: " + section + ", Price per hour: " + price + ", " + availability;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spot spot = (Spot) o;
+        return number == spot.number &&
+                floor == spot.floor &&
+                section.equals(spot.section) &&
+                Double.compare(spot.price, price) == 0 &&
+                availability.equals(spot.availability);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, floor, section, price, availability);
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public long getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+}

4861d846a14d7bc9f93ced265aee9017cf1ccd32
Cosmin-99 <susara.cosmin@gmail.com>
2020-06-16 15:44:45 +0300

    Added driver details page and route there

--- src/main/java/FIS/Project/Parkify/Controllers/ViewDriver.java
+++ src/main/java/FIS/Project/Parkify/Controllers/ViewDriver.java
@@ -1,4 +1,38 @@
 package FIS.Project.Parkify.Controllers;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class ViewDriver {
+
+    @FXML
+    public Text text;
+
+    @FXML
+    public void GoBack(){
+        try{
+            URL url = new File("src/main/java/FIS/Project/Parkify/FXML/ManagerMenu.fxml").toURI().toURL();
+
+            Stage stage = (Stage) text.getScene().getWindow();
+            Parent viewStudentsRoot = FXMLLoader.load(url);
+            Scene scene = new Scene(viewStudentsRoot, 1000, 640);
+            stage.setScene(scene);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void GoCancel(){
+        Platform.exit();
+    }
 }
